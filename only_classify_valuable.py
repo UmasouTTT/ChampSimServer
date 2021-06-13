e33173a03a519a1f131534993d2289864ec7254b
umasou <392660318@qq.com>
2021-06-13 19:59:45 +0800

    add ipcp + multiline + classify

--- only_classify_valuable.py
+++ only_classify_valuable.py
@@ -145,7 +145,7 @@ if __name__ == '__main__':
     ip_valuable_analysisor = "bimodal-no-paper_ipcp_value-paper_ipcp_value-no-lru-1core"
     #prefetcher = "bimodal-no-ip_classifier_v2_value_ip-ip_classifier_v1-no-lru-1core"
     #prefetcher = "bimodal-no-classifier_v3_only_classify-no-no-lru-1core"
-    ip_classify_paper = "bimodal-no-valuless_pattern-paper_ipcp_value-no-lru-1core"
+    ip_classify_paper = "bimodal-no-paper_ipcp_ip_classify_septrain-valuless_pattern_on_valuless-no-lru-1core"
     #ip_classify_paper_compare = "bimodal-no-paper_ipcp_ip_classify_v1-paper_ipcp-no-lru-1core"
 
     #build
@@ -161,8 +161,8 @@ if __name__ == '__main__':
     print("Start compile {} {} {} {} {} {} {}...".format(branch_predicor, l1i_prefetcher, l1d_prefetcher, l2c_prefetcher, llc_prefetcher, llc_replacement, core_num))
     compile_prefetcher(branch_predicor, l1i_prefetcher, l1d_prefetcher, l2c_prefetcher, llc_prefetcher, llc_replacement, core_num)
 
-    l1d_prefetcher = "valuless_pattern"
-    l2c_prefetcher = "paper_ipcp_value"
+    l1d_prefetcher = "paper_ipcp_ip_classify_septrain"
+    l2c_prefetcher = "valuless_pattern_on_valuless"
     compile_prefetcher(branch_predicor, l1i_prefetcher, l1d_prefetcher, l2c_prefetcher, llc_prefetcher, llc_replacement, core_num)
 
 
