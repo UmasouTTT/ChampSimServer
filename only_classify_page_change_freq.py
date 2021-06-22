6c766642ac0f5ea2b9a9ca3392606a063d483794
umasou <392660318@qq.com>
2021-06-22 16:07:35 +0800

    mix time

--- only_classify_page_change_freq.py
+++ only_classify_page_change_freq.py
@@ -97,7 +97,7 @@ if __name__ == '__main__':
     n_warm = 1
     n_sim = 10
     ip_valuable_analysisor = "bimodal-no-ip_page_change_frequency-no-no-lru-1core"
-    ip_classify_paper = "bimodal-no-ipcp_mix_time_absolute_classify-ipcp_mix_time_absolute_classify-no-lru-1core"
+    ip_classify_paper = "bimodal-no-ipcp_mix_time-ipcp_mix_time-no-lru-1core"
     #ip_classify_paper_compare = "bimodal-no-paper_ipcp_ip_classify_v1-paper_ipcp-no-lru-1core"
 
     #build
@@ -113,8 +113,8 @@ if __name__ == '__main__':
     print("Start compile {} {} {} {} {} {} {}...".format(branch_predicor, l1i_prefetcher, l1d_prefetcher, l2c_prefetcher, llc_prefetcher, llc_replacement, core_num))
     compile_prefetcher(branch_predicor, l1i_prefetcher, l1d_prefetcher, l2c_prefetcher, llc_prefetcher, llc_replacement, core_num)
 
-    l1d_prefetcher = "ipcp_mix_time_absolute_classify"
-    l2c_prefetcher = "ipcp_mix_time_absolute_classify"
+    l1d_prefetcher = "ipcp_mix_time"
+    l2c_prefetcher = "ipcp_mix_time"
     compile_prefetcher(branch_predicor, l1i_prefetcher, l1d_prefetcher, l2c_prefetcher, llc_prefetcher, llc_replacement, core_num)
 
     #make experiment
